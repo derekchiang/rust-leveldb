ba8b3a8c8e66e2295654d3fc8fc70d204acc1a27
Derek Chiang <derekchiang93@gmail.com>
2013-12-26 17:18:04 +0800

    Implementing more APIs

--- src/leveldb/lib.rs
+++ src/leveldb/lib.rs
@@ -30,7 +30,7 @@ pub mod options {
     pub enum OpenOption {
         CREATE_IF_MISSING,
         ERROR_IF_EXISTS,
-        PARONOID_CHECKS,
+        PARANOID_CHECKS,
     }
 
     pub enum WriteOption {
@@ -44,6 +44,8 @@ pub mod options {
     }
 }
 
+pub type WriteBatch<'r> = &'r [(&'r [u8], &'r [u8])];
+
 /// A database object
 pub struct DB {
     db: *mut leveldb_t
@@ -51,19 +53,64 @@ pub struct DB {
 
 pub type error = ~str;
 
+fn to_c_open_options(options: &[OpenOption]) -> *leveldb_options_t {
+    unsafe {
+        let c_options = leveldb_options_create();
+        for option in options.iter() {
+            match *option {
+                CREATE_IF_MISSING => {
+                    leveldb_options_set_create_if_missing(c_options, 1u8);
+                },
+                ERROR_IF_EXISTS => {
+                    leveldb_options_set_error_if_exists(c_options, 1u8);
+                },
+                PARANOID_CHECKS => {
+                    leveldb_options_set_paranoid_checks(c_options, 1u8);
+                }
+            }
+        }
+        c_options as *leveldb_options_t
+    }
+}
+
+fn to_c_write_options(options: &[WriteOption]) -> *leveldb_writeoptions_t {
+    unsafe {
+        let c_options = leveldb_writeoptions_create();
+        for option in options.iter() {
+            for option in options.iter() {
+                match *option {
+                    SYNC => {
+                        leveldb_writeoptions_set_sync(c_options, 1u8);
+                    }
+                }
+            }
+        }
+        c_options as *leveldb_writeoptions_t
+    }
+}
+
+fn to_c_read_options(options: &[ReadOption]) -> *leveldb_readoptions_t {
+    unsafe {
+        let c_options = leveldb_readoptions_create();
+        for option in options.iter() {
+            match *option {
+                VERIFY_CHECKSUM => {
+                    leveldb_readoptions_set_verify_checksums(c_options, 1u8);
+                },
+                FILL_CACHE => {
+                    leveldb_readoptions_set_fill_cache(c_options, 1u8);
+                }
+            }
+        }
+        c_options as *leveldb_readoptions_t
+    }
+}
+
 impl DB {
     /// Open a database connection
     pub fn open(name: &str, options: &[OpenOption]) -> Result<~DB, error> {
         unsafe {
-            let c_options = leveldb_options_create();
-            for option in options.iter() {
-                match *option {
-                    CREATE_IF_MISSING => {
-                        leveldb_options_set_create_if_missing(c_options, 1u8);
-                    },
-                    _ => {}
-                }
-            }
+            let c_options = to_c_open_options(options);
             let mut err: *mut c_char = mut_null();
             let c_db = leveldb_open(c_options as *leveldb_options_t,
                 name.to_c_str().unwrap(),
@@ -86,20 +133,12 @@ impl DB {
 
     pub fn put(&self, key: &[u8], value: &[u8], options: &[WriteOption]) -> Result<(), error> {
         unsafe {
-            let c_options = leveldb_writeoptions_create();
-            for option in options.iter() {
-                match *option {
-                    SYNC => {
-                        leveldb_writeoptions_set_sync(c_options, 1u8);
-                    }
-                }
-            }
             let mut c_err: *mut c_char = mut_null();
             let c_key = key.to_c_str();
             let c_key_len = c_key.len();
             let c_value = value.to_c_str();
             let c_value_len = c_value.len();
-            leveldb_put(self.db, c_options as *leveldb_writeoptions_t,
+            leveldb_put(self.db, to_c_write_options(options),
                 c_key.unwrap(), c_key_len as size_t,
                 c_value.unwrap(), c_value_len as size_t,
                 to_mut_unsafe_ptr(&mut c_err));
@@ -113,22 +152,11 @@ impl DB {
 
     pub fn get(&self, key: &[u8], options: &[ReadOption]) -> Result<~[u8], error> {
         unsafe {
-            let c_options = leveldb_readoptions_create();
-            for option in options.iter() {
-                match *option {
-                    VERIFY_CHECKSUM => {
-                        leveldb_readoptions_set_verify_checksums(c_options, 1u8);
-                    },
-                    FILL_CACHE => {
-                        leveldb_readoptions_set_fill_cache(c_options, 1u8);
-                    }
-                }
-            }
             let mut c_err: *mut c_char = mut_null();
             let c_key = key.to_c_str();
             let c_key_len = c_key.len();
             let mut c_value_len: size_t = 0;
-            let c_value = leveldb_get(self.db, c_options as *leveldb_readoptions_t,
+            let c_value = leveldb_get(self.db, to_c_read_options(options),
                 c_key.unwrap(), c_key_len as size_t,
                 to_mut_unsafe_ptr(&mut c_value_len),
                 to_mut_unsafe_ptr(&mut c_err));
@@ -137,6 +165,117 @@ impl DB {
             } else {
                 return Ok(from_buf_raw(c_value as *u8, c_value_len as uint));
             }
+        }
+    }
+
+    pub fn delete(&self, key: &[u8], options: &[WriteOption]) -> Result<(), error> {
+        unsafe {
+            let mut c_err: *mut c_char = mut_null();
+            let c_key = key.to_c_str();
+            let c_key_len = c_key.len();
+            leveldb_delete(self.db, to_c_write_options(options),
+                c_key.unwrap(), c_key_len as size_t,
+                to_mut_unsafe_ptr(&mut c_err));
+            if is_not_null(c_err) {
+                return Err(from_c_str(c_err as *c_char));
+            } else {
+                return Ok(());
+            }
+        }
+    }
+
+    pub fn write(&self, write_batch: WriteBatch, options: &[WriteOption]) -> Result<(), error> {
+        unsafe {
+            let c_write_batch = leveldb_writebatch_create();
+            for &(key, value) in write_batch.iter() {
+                let c_key = key.to_c_str();
+                let c_key_len = c_key.len();
+                let c_value = value.to_c_str();
+                let c_value_len = c_value.len();
+                leveldb_writebatch_put(c_write_batch,
+                    c_key.unwrap(), c_key_len as size_t,
+                    c_value.unwrap(), c_value_len as size_t);
+            }
+            let mut c_err: *mut c_char = mut_null();
+            leveldb_write(self.db, to_c_write_options(options),
+                c_write_batch, to_mut_unsafe_ptr(&mut c_err));
+            if is_not_null(c_err) {
+                return Err(from_c_str(c_err as *c_char));
+            } else {
+                return Ok(());
+            }
+        }
+    }
+
+    pub fn iter(&self, options: &[ReadOption]) -> DBIterator {
+        unsafe {
+            let it = leveldb_create_iterator(self.db, to_c_read_options(options));
+            leveldb_iter_seek_to_first(it);
+            return DBIterator{
+                iter: it
+            }
+        }
+    }
+}
+
+pub struct DBIterator {
+    iter: *mut leveldb_iterator_t
+}
+
+
+impl Iterator<(~[u8], ~[u8])> for DBIterator {
+    fn next(&mut self) -> Option<(~[u8], ~[u8])> {
+        unsafe {
+            if leveldb_iter_valid(self.iter as *leveldb_iterator_t) == 0u8 {
+                return None;
+            } else {
+                let pair = (self.key(), self.value());
+                leveldb_iter_next(self.iter);
+                return Some(pair);
+            }
+        }
+    }
+}
+
+impl DBIterator {
+    fn prev(&mut self) {
+        
+    }
+
+    fn key(&self) -> ~[u8] {
+        unsafe {
+            let mut c_key_len: size_t = 0;
+            let c_key = leveldb_iter_key(self.iter as *leveldb_iterator_t,
+                to_mut_unsafe_ptr(&mut c_key_len));
+            from_buf_raw(c_key as *u8, c_key_len as uint)
+        }
+    }
+
+    fn value(&self) -> ~[u8] {
+        unsafe {
+            let mut c_val_len: size_t = 0;
+            let c_val = leveldb_iter_value(self.iter as *leveldb_iterator_t,
+                to_mut_unsafe_ptr(&mut c_val_len));
+            from_buf_raw(c_val as *u8, c_val_len as uint)
+        }
+    }
+
+    fn get_error(&self) -> Option<error> {
+        unsafe {
+            let mut c_err: *mut c_char = mut_null();
+            leveldb_iter_get_error(self.iter as *leveldb_iterator_t,
+                to_mut_unsafe_ptr(&mut c_err));
+            if is_not_null(c_err) {
+                return Some(from_c_str(c_err as *c_char));
+            } else {
+                return None;
+            }
+        }
+    }
+
+    fn is_valid(&self) -> bool {
+        unsafe {
+            return leveldb_iter_valid(self.iter as *leveldb_iterator_t) != 0u8;
         }
     }
 }
--- src/leveldb/test.rs
+++ src/leveldb/test.rs
@@ -10,6 +10,9 @@ fn test_db_open() {
     let res = DB::open("db", [options::CREATE_IF_MISSING]);
     match res {
         Ok(db) => {
+            for (key, value) in db.iter([]) {
+                println!("key: {}, val: {}", from_utf8(key), from_utf8(value));
+            }
             let res = db.put("foo".as_bytes(), "bar".as_bytes(), []);
             match res {
                 Ok(_) => {},
